10615e5dc37684e756e79d025c77826cee730650
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2022-08-26 10:13:14 -0400

    using images from this repo now

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   return <><Globe
     ref={globeEl}
     globeMaterial={globeMaterial}
-    globeImageUrl={`https://raw.githubusercontent.com/MathewBiddle/bio_ice/main/map_obis_${taxa}.jpg`}
+    globeImageUrl={`https://raw.githubusercontent.com/MathewBiddle/gbif_globe/master/images/map_obis_${taxa}.jpg`}
 //    bumpImageUrl={`https://object-arbutus.cloud.computecanada.ca/bq-io/io/gbif-heatmaps/earth-topology.jpg#{taxa}`}
     backgroundImageUrl="https:////unpkg.com/three-globe/example/img/night-sky.png" />
     //<SideNav
