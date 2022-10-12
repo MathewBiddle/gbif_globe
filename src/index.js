5fc6f66f6166489c96bec5e5e8f8ae6150f1e3e3
Mathew.Biddle <mathew.biddle@noaa.gov>
2022-10-12 14:18:51 -0400

    adding title and legend

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div style={{position:'absolute',top:"5px",center:'100px',color:'white',zIndex:999,fontSize:'14px'}}><h1>Ocean Biodiversity Information System (OBIS) ES50 Indicator</h1></div>
+    <div style={{position:'absolute',bottom:"30px",right:'20px',color:'white',zIndex:999,fontSize:'14px'}}><img src="https://github.com/MathewBiddle/globe/raw/main/images/globe/legend.jpg" alt="Legend" /></div>
     <div style={{position:'absolute',bottom:"5px",right:'20px',color:'white',zIndex:999,fontSize:'14px'}}>TEXT NEEDS TO BE UPDATED <a style={{color:'white'}} href="https://github.com/MathewBiddle/gbif_globe" target="_blank">GitHub Repository</a></div>
   </React.StrictMode>
 );
